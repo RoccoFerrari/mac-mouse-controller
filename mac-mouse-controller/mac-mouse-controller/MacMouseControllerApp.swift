83700c3f820fd8c290a626fe7c60916fa8f2fb80
Rocco <emsicoding@gmail.com>
2026-01-25 11:38:07 +0100

    init: initialize very first setup commit

---
+++ mac-mouse-controller/mac-mouse-controller/MacMouseControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_mouse_controllerApp.swift
+//  mac-mouse-controller
+//
+//  Created by Rocco Ferrari on 25/01/26.
+//
+
+import SwiftUI
+
+@main
+struct mac_mouse_controllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
