7fffa97ed431998f1dd9bd4f2a7cc322af38ee1a
Rocco <emsicoding@gmail.com>
2026-01-27 17:04:52 +0100

    fix: update saving persistently logic

--- mac-mouse-controller/Models/MappingProfile.swift
+++ mac-mouse-controller/Models/MappingProfile.swift
@@ -23,14 +23,23 @@ struct MappingRule: Identifiable, Codable {
 
 /// Class containing a set of rules for a user
 class UserProfile: ObservableObject, Codable {
-    @Published var rules: [MappingRule] = []
+    @Published var rules: [MappingRule] = [] {
+        didSet {
+            saveToDisk()
+        }
+    }
+    
+    // Key to save data
+    private let storageKey = "MouseController_Rules_V1"
     
     // Method neede to support @Published with Codable
     enum CodingKeys: CodingKey {
         case rules
     }
     
-    init() {}
+    init() {
+        loadFromDisk()
+    }
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -40,5 +49,33 @@ class UserProfile: ObservableObject, Codable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(rules, forKey: .rules)
+    }
+    
+    // MARK: Persistence Logic
+    private func saveToDisk() {
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(self)
+            UserDefaults.standard.set(data, forKey: storageKey)
+            print("Configurationsaved: \(rules.count) rules")
+        } catch {
+            print("Failed to save rules: \(error)")
+        }
+    }
+    
+    private func loadFromDisk() {
+        guard let data = UserDefaults.standard.data(forKey: storageKey) else {
+            print("No saved configurations")
+            return
+        }
+        
+        do {
+            let encoder = JSONDecoder()
+            let savedRules = try encoder.decode(UserProfile.self, from: data)
+            self.rules = savedRules.rules
+            print("Configurations loaded: \(rules.count) rules")
+        } catch {
+            print("Failed to load rules: \(error)")
+        }
     }
 }
